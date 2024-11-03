85d363003564c21368849d950fd74ef1e9bc0642
Alexrazumovich <alexrazumovich@gmail.com>
2024-11-04 01:35:19 +0200

    weather

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import asyncio
+import asyncio,requests
 import random
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart,Command
@@ -31,6 +31,31 @@ async def photo(message: Message):
     # list = [ссылки URL на изображения через запятую]
     rand_photo = random.choice(LIST)
     await message.answer_photo(photo=rand_photo, caption='Это супер крутая картинка')
+@dp.message(Command('weather'))
+async def weater(message: Message):
+    await message.answer("Введите название города")
+
+@dp.message()
+async def weather_in_city(message: Message):
+    city = message.text
+    weather=get_weather(city)
+    if weather.get("cod") != 200:
+        await message.answer(f"Не удалось получить погоду для города {city}. Проверьте правильность написания.")
+    else:
+        str = (
+            f"Прогноз погоды в {weather['name']}:\n"
+            f"Температура: {weather['main']['temp']}°C\n"
+            f"Описание погоды: {weather['weather'][0]['description']}"
+        )
+        await message.answer(str)
+
+
+def get_weather(city):
+    api_key = '83ae226b645257424cfc78b3aa9e1aed'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    response = requests.get(url)
+    return response.json()
+
 
 async def main():
     await dp.start_polling(bot)
