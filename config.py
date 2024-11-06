50e511347e42fdca87210cc754b16818ca476e28
Alexrazumovich <alexrazumovich@gmail.com>
2024-11-07 01:02:40 +0200

    new_weather

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 TOKEN='7611620776:AAEYW90Yz6RG_mwZ0cbalPwmqeLelSISX3g'
+WEATHER_API_KEY='83ae226b645257424cfc78b3aa9e1aed'
 LIST=[
 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=600',
 'https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg?auto=compress&cs=tinysrgb&w=600',
